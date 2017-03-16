2b2a63c57fcddd0278ef4e414e0c530b8c727ea6
nitrodragon <nitrodragon@users.noreply.github.com>
2017-03-15 21:12:11 -0700

    Update Calculator.java
    Removed Unused Imports

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,14 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.math.BigDecimal;
